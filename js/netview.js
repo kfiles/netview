a887e2c556d0d3271710a9f45b911fb0b30d10ab
Roberto Arrocha <roberto.arrocha@tx.rr.com>
2009-08-22 01:37:52 +0000

    Removing "Initial commit" files from root directory.

--- js/netview.js
+++
@@ -1,9 +0,0 @@
-/*
-Title: NetView Javascript
-*/
- 
-@import url(javascript.js);
-@import url(SpryAssets/xpath.js);
-@import url(SpryAssets/SpryMenuBar.js);
-@import url(SpryAssets/SpryData.js);
-@import url(SpryAssets/SpryNestedXMLDataSet.js);
