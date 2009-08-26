f6b305a6f1749de6b3ee654d3cd9d5f102eabea7
Kirby Files <ksfiles@gmail.com>
2009-08-26 01:35:13 +0000

    Added Preferences module

---
+++ netview/src/com/prodco/preferences/client/PreferencesEntry.java
@@ -0,0 +1,15 @@
+package com.prodco.preferences.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class PreferencesEntry implements EntryPoint
+  {
+
+  @Override
+  public void onModuleLoad ()
+    {
+    // TODO Auto-generated method stub
+
+    }
+
+  }
